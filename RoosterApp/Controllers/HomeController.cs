325fdbd6ffdac88b40af10e1ea51b14935065ce5
ThijsVredenbregt <wel5linGH>
2015-03-13 17:24:05 +0100

    added jquery background infro update to status page

--- RoosterApp/Controllers/HomeController.cs
+++ RoosterApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RoosterApp.Models;
 
 namespace RoosterApp.Controllers
 {
@@ -18,8 +19,21 @@ namespace RoosterApp.Controllers
 
         public ActionResult Status()
         {
+            IEnumerable<StatusImgModel> list = Repository.GetData();
+            return View(list);
+        }
 
-            return View();
+        public PartialViewResult StatusImgPartialView()
+        {
+            IEnumerable<StatusImgModel> list = Repository.GetData();
+
+            int min = list.Min(m => m.ID);
+            int max = list.Max(m => m.ID);
+
+            int randomId = new Random().Next(min, (max + 1));
+
+            StatusImgModel model = list.FirstOrDefault(m => m.ID == randomId);
+            return PartialView("_StatusImgPartial", model);
         }
 
     }
---
+++ RoosterApp/Models/StatusImgModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoosterApp.Models
+{
+    public class StatusImgModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+    public class Repository
+    {
+        public static List<StatusImgModel> GetData()
+        {
+            List<StatusImgModel> list = new List<StatusImgModel>
+            {
+                new StatusImgModel
+                {
+                    ID = 1,
+                    Name = "default",
+                    ImageUrl = "../../Images/heartbeat2.png"
+                },
+                new StatusImgModel
+                {
+                    ID = 2,
+                    Name = "Online",
+                    ImageUrl = "../../Images/heartbeat_green2.png"
+                },
+                new StatusImgModel
+                {
+                    ID = 3,
+                    Name = "Offline",
+                    ImageUrl = "../../Images/heartbeat_red2.png"
+                }
+            };
+
+            return list;
+        }
+    }
+}
