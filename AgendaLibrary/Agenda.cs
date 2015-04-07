68d54c540088357d0bf29db3c7d531db8b9205b4
Thijs Vredenbregt <thijs+github@vredenbregt.net>
2015-04-07 12:29:33 +0200

    agenda agent update

---
+++ AgendaLibrary/Agenda.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Oauth2.v2.Data;
+
+namespace AgendaLibrary
+{
+    public class Agenda
+    {
+        private CalendarService _service;
+        private CalendarService _userService;
+        private Userinfoplus _userInfo;
+        private Events _agenda;
+        public Agenda()
+        {
+            _service = Network.GetCalendar();
+            _userService = Network.GetUserCalendarService();
+            _userInfo = Network.GetUserInfoService();
+            _agenda = Network.GetAgenda();
+        }
+
+        public Boolean AddEvent()
+        {
+            var event1 = new Event()
+            {
+                Id = "362606de6aa34923815ffe0dc4616023",
+                Summary = "CONTROLE AAN DE KANT !! DIT IS DE BAZEN POLITIE ",
+                Creator = new Event.CreatorData(){DisplayName = "Roosterify", Email = "roosterify@gmail.com"},               
+                Organizer = new Event.OrganizerData() { DisplayName = "Roosterify", Email = "roosterify@gmail.com" },
+                Start = new EventDateTime() { DateTime = DateTime.Today},
+                End = new EventDateTime() { DateTime = DateTime.Now.AddDays(1)},
+                Description = "WHAT SEEMS TO BE THE OFFICER PROBLEM ?",
+            };
+
+
+            var attendeeList = new List<EventAttendee>();
+            attendeeList.Add(new EventAttendee()
+            {
+                Email = _userInfo.Email,
+                ResponseStatus = "accepted",
+                Self = true,
+                Organizer = false
+            });
+
+            EventAttendee ea = new EventAttendee();
+            
+            event1.Attendees = attendeeList;
+            _service.Events.Insert(event1, Network._agendaId).Execute();
+
+            return true;
+        }
+
+        public CalendarService GetUserCalendarService()
+        {
+            return _userService;
+        }
+
+        public Events GetUserAgenda()
+        {
+            return _userService.Events.List("primary").Execute();
+        }
+
+        public bool UpdateEvent(Les les)
+        {
+            try
+            {
+                var item = _agenda.Items.FirstOrDefault(x => x.Id.Equals(les.CalendarGuid));
+
+                List<EventAttendee> lea = item.Attendees.ToList();
+
+                if (lea.Any(x=>x.Email.Equals(_userInfo.Email)))
+                {
+                    lea.Add(new EventAttendee()
+                    {
+                        Email = _userInfo.Email,
+                        ResponseStatus = "accepted",
+                        Self = true,
+                        Organizer = false
+                    });
+                }
+
+                item.Attendees = lea;
+
+                _service.Events.Update(item, Network._agendaId, les.CalendarGuid).Execute();
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+    }
+}
---
+++ AgendaLibrary/Network.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Oauth2.v2.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace AgendaLibrary
+{
+
+    public static class Network
+    {
+        private static readonly string _serviceAccountEmail = "177600524325-6sbfabk2ldv0ickbpjtvfjqbieagg289@developer.gserviceaccount.com";
+        private static readonly string _privateKeyPassword = "notasecret";
+        internal static readonly string _agendaId = "roosterify@gmail.com";
+        private static readonly string ApplicationName = "Roosterify";
+        private static Events _agenda;
+        private static CalendarService _service;
+        private static CalendarService _userService;
+        private static Userinfoplus _userInfo;
+
+
+        static Network()
+        {
+            _service = GoogleCalendar();
+            GetUserService();
+        }
+
+        private static void GetUserService()
+        {
+            string ClientId = "177600524325-le2ar5m4qoffvdadptka06gkpc92eht6.apps.googleusercontent.com";
+            string ClientSecret = "a4u6ROKjicMbegGAmYzJLlEP";
+
+            UserCredential credential2 = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets { ClientId = ClientId, ClientSecret = ClientSecret },
+                new[] { CalendarService.Scope.Calendar, Google.Apis.Oauth2.v2.Oauth2Service.Scope.UserinfoEmail},
+                "user", 
+                CancellationToken.None,
+                new FileDataStore("Drive.Auth.Store")
+            ).Result;
+
+            var oauthSerivce = new Google.Apis.Oauth2.v2.Oauth2Service(
+                new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential2,
+                    ApplicationName = ApplicationName,
+                });
+
+            _userInfo = oauthSerivce.Userinfo.Get().Execute();
+
+            _userService = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential2,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        private static CalendarService GoogleCalendar()
+        {
+            var certificate = new X509Certificate2(Properties.Resources.Roosterify_b75ac5863d29, _privateKeyPassword, X509KeyStorageFlags.Exportable);
+
+            var credential = new ServiceAccountCredential(
+                new ServiceAccountCredential.Initializer(_serviceAccountEmail)
+                {
+                    Scopes = new[] { CalendarService.Scope.Calendar }
+                }.FromCertificate(certificate));
+
+            return new CalendarService(
+                new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName
+                });
+        }
+
+        public static bool GetData()
+        {
+            _service = GoogleCalendar();
+            _agenda = _service.Events.List(_agendaId).Execute();
+            return true;
+        }
+
+        public static Events GetAgenda()
+        {
+            return _agenda;
+        }
+
+        public static CalendarService GetCalendar()
+        {
+            return _service;
+        }
+
+        public static CalendarService GetUserCalendarService()
+        {
+            return _userService;
+        }
+
+        public static Userinfoplus GetUserInfoService()
+        {
+            return _userInfo;
+        }
+
+    }
+}
